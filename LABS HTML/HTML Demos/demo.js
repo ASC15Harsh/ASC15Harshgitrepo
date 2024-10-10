0ed3fae2ab4a1309c50d5030274ffbd769ca5622
Harsh Mishra <harsh.mishra@ascendion.com>
2024-10-10 15:24:16 +0530

    Training

---
+++ LABS HTML/HTML Demos/demo.js
@@ -0,0 +1,83 @@
+console.log("Welcome");
+
+
+fullname ="Tony Stark";
+console.log(fullname);
+
+x=null;
+console.log(x);
+
+y=undefined
+console.log(y);
+
+const names = "Harsh"
+console.log(names);
+
+let age = 22
+console.log(age);
+
+// Datatypes
+// Number,string, Boolean,Undefined,Null,BigInt,Symbol
+
+const student = {
+    fullName : "Harsh M",
+    age : 22,
+    cgpa : 20,
+};
+console.log(student);
+console.log(student["fullName"]);
+console.log(student.age);
+student.age = student.age + 1;
+console.log(student.age);
+
+let a=5;
+let b=4;
+
+//arithematic operator
+// console.log("a + b = ",a+b);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// console.log("a % b = ",a%b);
+// console.log("a ** b = ",a**b);
+
+
+//unary operator
+//post - a++ ||||||  pre - ++a  
+// console.log("a++ = ",++a);
+// console.log("b-- = ",--b);
+
+
+//assignment operator
+// = , += , -= , *=  , %= , **=
+
+//comparision operator
+//== , === , != , !== , > , <= , > , >= 
+
+//logical operators
+// && , || , ! 
+
+
+//conditional statement
+// let agge = 15
+// if(agge >= 18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+
+
+//alert("hello !");
+
+// let num= prompt("Enter a number")
+// if(num % 5 == 0){
+//     console.log("number divisible by 5");
+// }
+// else{
+//     console.log("not divisible by 5");
+// }
+
+
+
+
